cb40a543d1316044c7d505675169e1a26e68b41a
Arnau Diaz <diazarnau0@gmail.com>
2023-12-06 16:00:41 +0100

    test: recreate table in dynamodb to easy clean up

--- src/test/java/org/notabug/ranking/repository/DynamoDbStorageTest.java
+++ src/test/java/org/notabug/ranking/repository/DynamoDbStorageTest.java
@@ -1,5 +1,6 @@
 package org.notabug.ranking.repository;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -26,17 +27,26 @@ public class DynamoDbStorageTest {
     DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient;
     @Autowired
     ReactiveCircuitBreakerFactory<?, ?> cbFactory;
+
+    DynamoDbAsyncTable<VoteDynamoDb> table;
     DynamoDbStorage dynamoDbStorage;
 
     @BeforeAll
     void setup() {
-        DynamoDbAsyncTable<VoteDynamoDb> table = dynamoDbEnhancedAsyncClient.table(
+        table = dynamoDbEnhancedAsyncClient.table(
                 VoteDynamoDb.VOTE_TABLE_NAME,
                 TableSchema.fromBean(VoteDynamoDb.class)
         );
         table.createTable();
         dynamoDbStorage = new DynamoDbStorage(cbFactory, table);
     }
+
+    @AfterEach
+    void recreateTable() {
+        table.deleteTable();
+        table.createTable();
+    }
+
 
     @Test
     public void whenPutUserThenSuccess() {
@@ -46,9 +56,31 @@ public class DynamoDbStorageTest {
     }
 
     @Test
-    // TODO stateless
     public void whenGetAllThenOneVote() {
+        StepVerifier.create(dynamoDbStorage.vote("user", 1, 1))
+                .expectComplete()
+                .verify();
+
         StepVerifier.create(dynamoDbStorage.getAll())
+                .expectNext(new VoteOut("user", 1, 1))
+                .expectComplete()
+                .verify();
+    }
+
+
+    @Test
+    public void whenGetAllThenTwoVotes() {
+        StepVerifier.create(dynamoDbStorage.vote("user", 1, 1))
+                .expectComplete()
+                .verify();
+
+        StepVerifier.create(dynamoDbStorage.vote("user", 1, 1))
+                .expectComplete()
+                .verify();
+
+
+        StepVerifier.create(dynamoDbStorage.getAll())
+                .expectNext(new VoteOut("user", 1, 1))
                 .expectNext(new VoteOut("user", 1, 1))
                 .expectComplete()
                 .verify();
